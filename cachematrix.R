d9a4f15f559a2bee6be82c4db35816ff940f9672
Slawomir Salamon <slawomir.salamon@vsof.pl>
2015-09-23 09:39:56 +0200

    adding speed test

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeMatrix functions builds a matrix and prepares interface
+## to manage cashed matrix object
 makeMatrix <- function(x = matrix()) {
     cacheMatrix <- NULL
     setMatrix <- function(y){
@@ -11,7 +13,7 @@ makeMatrix <- function(x = matrix()) {
     getMatrix <- function() x
     
     setCacheMatrix <- function(setMatrixInverse)
-        cacheMatrix <- setMatrixInverse
+        cacheMatrix <<- setMatrixInverse
     getCacheMatrix <- function() cacheMatrix
     
     list(setMatrix = setMatrix,
@@ -21,8 +23,28 @@ makeMatrix <- function(x = matrix()) {
 }
 
 ## Write a short comment describing this function 
-
+## cacheSolve returns inversed square matrix form the cache
+## if there is no matrix in the cache cacheSolve creates one
+## and stores it in the cache
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    
+    invMatrix <- x$getCacheMatrix()
+    if(!is.null(invMatrix)){
+        message("getting cached matrix")
+        return(invMatrix)
+    }
+    matrixData <- x$getMatrix()
+    temp <- solve(matrixData)
+    x$setCacheMatrix(temp)
+    temp
 }
+
+
+## speed test
+speedMatrixTest <- function(size){
+    hugeMatrix <- makeMatrix(matrix(rnorm(size), ncol = sqrt(size)))
+    system.time(cacheSolve(hugeMatrix))
+    system.time(cacheSolve(hugeMatrix))
+}
+
+
